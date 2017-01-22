3d44ffcb646530574c29f5d859f0f4aa86cbebc4
Colin Socha <colin.socha@DSKTP-1107.fios-router.home>
2017-01-22 15:35:56 -0500

    Breweries by city/state

--- test.py
+++ test.py
@@ -8,14 +8,25 @@ def getLocationByZip(zip):
   locatonUrl = '{0}/locations'.format(url)
   params['postalCode'] = zip
   response = requests.get(locatonUrl, params=params)
-  return(json.loads(response.text))
+  parsed = json.loads(response.text)
+  #todo handle multiple pages
+  return(parsed['data'])
 
-concordZips = ['94521','94522','94518','94519','94520','94524','94527','94529']
-concordBreweries = {}
+def getLocationByCityState(city, state):
+  locatonUrl = '{0}/locations'.format(url)
+  params['locality'] = city
+  params['region'] = state
+  response = requests.get(locatonUrl, params=params)
+  parsed = json.loads(response.text)
+  #todo handle multiple pages
+  return(parsed['data'])
 
-for zip in concordZips:
-  concordBreweries[zip] = getLocationByZip(zip)
+#concordZips = ['94521','94522','94518','94519','94520','94524','94527','94529']
+concordLocations = getLocationByCityState('Concord','California')
+for loc in concordLocations:
+ if 'brewery' in loc:
+   print  loc['brewery']['name']
 
-print json.dumps(concordBreweries, indent=4, sort_keys=True)
+#print json.dumps(concordLocations, indent=4, sort_keys=True)
 
 
